5ce3c525b33e3fb6f409c699a6a0cb6495a9b016
Sérgio Mafra <sergio@mafra.io>
2019-02-13 02:50:27 -0200

    Lesson 08 topic 11 conclusion

---
+++ Lesson_07/topic18/topic18.py
@@ -0,0 +1,65 @@
+import pickle
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Read in the saved camera matrix and distortion coefficients
+# These are the arrays you calculated using cv2.calibrateCamera()
+dist_pickle = pickle.load( open( "wide_dist_pickle.p", "rb" ) )
+mtx = dist_pickle["mtx"]
+dist = dist_pickle["dist"]
+
+# Read in an image
+img = cv2.imread('test_image2.png')
+nx = 8 # the number of inside corners in x
+ny = 6 # the number of inside corners in y
+
+# MODIFY THIS FUNCTION TO GENERATE OUTPUT
+# THAT LOOKS LIKE THE IMAGE ABOVE
+def corners_unwarp(img, nx, ny, mtx, dist):
+    # Pass in your image into this function
+    # Write code to do the following steps
+    # 1) Undistort using mtx and dist
+    undst = cv2.undistort(img, mtx, dist, None, mtx)
+    # 2) Convert to grayscale
+    gray = cv2.cvtColor(undst, cv2.COLOR_BGR2GRAY)
+    # 3) Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+    # 4) If corners found:
+    if ret == True:
+        # a) draw corners
+        # Draw and display the corners
+        undst = cv2.drawChessboardCorners(undst, (nx, ny), corners, ret)
+        plt.imshow(undst)
+        # b) define 4 source points src = np.float32([[,],[,],[,],[,]])
+        #Note: you could pick any four of the detected corners
+        # as long as those four corners define a rectangle
+        #One especially smart way to do this would be to use four well-chosen
+        # corners that were automatically detected during the undistortion steps
+        #We recommend using the automatic detection of corners in your code
+        src_points = np.float32( [corners[0][0],
+                                  corners[7][0],
+                                  corners[-8][0],
+                                  corners[-1][0]] )
+        # c) define 4 destination points dst = np.float32([[,],[,],[,],[,]])
+        dst_points = np.float32( [[200,200],
+                                  [1000,200],
+                                  [200,800],
+                                  [1000,800]] )
+        # d) use cv2.getPerspectiveTransform() to get M, the transform matrix
+        M = cv2.getPerspectiveTransform(src_points, dst_points)
+        # e) use cv2.warpPerspective() to warp your image to a top-down view
+        warped = cv2.warpPerspective(undst, M, (undst.shape[1], undst.shape[0]),
+                flags=cv2.INTER_LINEAR)
+
+    return warped, M
+
+top_down, perspective_M = corners_unwarp(img, nx, ny, mtx, dist)
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(img)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(top_down)
+ax2.set_title('Undistorted and Warped Image', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
